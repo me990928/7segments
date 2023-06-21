84ee91f22e3e94aa3cdc67fb58c887e149900c64
me990928 <me990928@gmail.com>
2023-06-21 22:57:48 +0900

    Initial Commit

---
+++ 7segments/_segmentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _segmentsApp.swift
+//  7segments
+//
+//  Created by Yuya Hirose on 2023/06/21.
+//
+
+import SwiftUI
+
+@main
+struct _segmentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
